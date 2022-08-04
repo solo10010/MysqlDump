77e84f52cd7eebdb1ac803eaa2e812f70806dafa
solo <50051085+solo10010@users.noreply.github.com>
2022-08-04 14:09:06 +0600

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-USER="Логин, который имеет доступ ко всем базам"
+USER="root"
 PASSWORD="Пароль"
 OUTPUT="/root/backup_mysql" #директория для хранения резервных копий
  
-databases=`mysql -u root -e "SHOW DATABASES;" | tr -d "|" | grep -v Database`
+databases=`mysql -u $USER -e "SHOW DATABASES;" | tr -d "|" | grep -v Database`
  
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != _* ]]; then
         echo "Dumping database: $db"
-        mysqldump --force --opt -u root --databases $db > $OUTPUT/`date +%Y%m%d`.$db.sql 
+        mysqldump --force --opt -u $USER --databases $db > $OUTPUT/`date +%Y%m%d`.$db.sql 
         gzip $OUTPUT/`date +%Y%m%d`.$db.sql
     fi
 done
