521a2c3825949681b8f4e230f0a63e9c261369ea
solo <50051085+solo10010@users.noreply.github.com>
2022-08-04 14:13:05 +0600

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -7,9 +7,9 @@ OUTPUT="/root/backup_mysql" #директория для хранения рез
 databases=`mysql -u $USER -e "SHOW DATABASES;" | tr -d "|" | grep -v Database`
  
 for db in $databases; do
-    if [[ "$db" != "information_schema" ]]; then
+
         echo "Dumping database: $db"
         mysqldump --force --opt -u $USER --databases $db > $OUTPUT/`date +%Y%m%d`.$db.sql 
         gzip $OUTPUT/`date +%Y%m%d`.$db.sql
-    fi
+
 done
