a8dc27a64fbe04617fc582e1e6628241c314b407
solo <50051085+solo10010@users.noreply.github.com>
2022-08-16 12:48:40 +0600

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-
-USER="root"
-PASSWORD="Пароль"
-OUTPUT="/root/backup_mysql" #директория для хранения резервных копий
- 
-databases=`mysql -u $USER -e "SHOW DATABASES;" | tr -d "|" | grep -v Database`
- 
-for db in $databases; do
-
-        echo "Dumping database: $db"
-        mysqldump --force --opt -u $USER --databases $db > $OUTPUT/`date +%Y%m%d`.$db.sql 
-        gzip $OUTPUT/`date +%Y%m%d`.$db.sql
-
+#!/bin/bash 
+USER="root" 
+PASSWORD="Пароль" 
+OUTPUT="/var/www/backup_mysql" #директория для хранения резервных копий 
+databases=`mysql -u $USER -e "SHOW DATABASES;" | tr -d "|" | grep -v Database` 
+for db in $databases; do 
+        echo "Dumping database: $db" 
+        mysqldump --single-transaction  --force --opt -u $USER --databases $db > $OUTPUT/`date +%Y%m%d`.$db.sql 
+        #gzip $OUTPUT/`date +%Y%m%d`.$db.sql 
 done
+
+mysqldump -u root --all-databases > $OUTPUT/ALLDATABASES.sql
